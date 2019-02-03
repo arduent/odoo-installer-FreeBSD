8e0ddea2b1d91e1f436ec675e087551755c79499
Charlie Root <root@oo1.arduent.com>
2019-02-03 22:06:03 +0000

    enablepf.sh

---
+++ enablepf.sh
@@ -0,0 +1,5 @@
+echo 'pf_enable="YES"' >> /etc/rc.conf
+echo 'pf_rules="/etc/pf.conf"' >> /etc/rc.conf
+echo 'pflog_enable="YES"' >> /etc/rc.conf
+echo 'pflog_logfile="/var/log/pflog"' >> /etc/rc.conf
+
